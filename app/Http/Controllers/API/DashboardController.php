e6362841f56665f424a07bfc25f58e0029d0f28d
fanebrahima <brahimafane06@gmail.com>
2025-10-30 12:05:38 +0000

    Update

--- app/Http/Controllers/API/DashboardController.php
+++ app/Http/Controllers/API/DashboardController.php
@@ -30,9 +30,9 @@ class DashboardController extends Controller
         // $this->authorize('viewAny', User::class);
 
         return $this->responseSuccess(null, [
-            'total_users' => ['value' => User::count()],
-            'active_users' => ['value' => User::where('status_id', Status::where('code', StatusEnum::ACTIVE)->first()->id)->count()],
-            'inactive_users' => ['value' => User::where('status_id', Status::where('code', StatusEnum::INACTIVE)->first()->id)->count()],
+            'total_users' => ['value' => User::accessibleBy(auth()->user())->count()],
+            'active_users' => ['value' => User::where('status_id', Status::where('code', StatusEnum::ACTIVE)->first()->id)->accessibleBy(auth()->user())->count()],
+            'inactive_users' => ['value' => User::where('status_id', Status::where('code', StatusEnum::INACTIVE)->first()->id)->accessibleBy(auth()->user())->count()],
         ]);
     }
 
@@ -44,17 +44,17 @@ class DashboardController extends Controller
         // $this->authorize('viewAny', Assignment::class);
 
         return $this->responseSuccess(null, [
-            'total_assignments' => ['value' => Assignment::count()],
-            'open_assignments' => ['value' => Assignment::where('status_id', Status::where('code', StatusEnum::OPENED)->first()->id)->count()],
-            'realized_assignments' => ['value' => Assignment::where('status_id', Status::where('code', StatusEnum::REALIZED)->first()->id)->count()],
+            'total_assignments' => ['value' => Assignment::accessibleBy(auth()->user())->count()],
+            'open_assignments' => ['value' => Assignment::where('status_id', Status::where('code', StatusEnum::OPENED)->first()->id)->accessibleBy(auth()->user())->count()],
+            'realized_assignments' => ['value' => Assignment::where('status_id', Status::where('code', StatusEnum::REALIZED)->first()->id)->accessibleBy(auth()->user())->count()],
             'edited_assignments' => ['value' => Assignment::where('status_id', Status::where('code', StatusEnum::EDITED)->first()->id)->count()],
-            'validated_assignments' => ['value' => Assignment::where('status_id', Status::where('code', StatusEnum::VALIDATED)->first()->id)->count()],
-            'paid_assignments' => ['value' => Assignment::where('status_id', Status::where('code', StatusEnum::PAID)->first()->id)->count()],
-            'closed_assignments' => ['value' => Assignment::where('status_id', Status::where('code', StatusEnum::CLOSED)->first()->id)->count()],
+            'validated_assignments' => ['value' => Assignment::where('status_id', Status::where('code', StatusEnum::VALIDATED)->first()->id)->accessibleBy(auth()->user())->count()],
+            'paid_assignments' => ['value' => Assignment::where('status_id', Status::where('code', StatusEnum::PAID)->first()->id)->accessibleBy(auth()->user())->count()],
+            'closed_assignments' => ['value' => Assignment::where('status_id', Status::where('code', StatusEnum::CLOSED)->first()->id)->accessibleBy(auth()->user())->count()],
             'cancelled_assignments' => ['value' => Assignment::where('status_id', Status::where('code', StatusEnum::CANCELLED)->first()->id)->count()],
 
-            'assignments_edition_time_expired' => Assignment::where('created_at', '<', Carbon::now()->subHours(24))->count(),
-            'assignments_recovery_time_expired' => Assignment::where('created_at', '<', Carbon::now()->subHours(48))->count(),
+            'assignments_edition_time_expired' => Assignment::where('created_at', '<', Carbon::now()->subHours(24))->accessibleBy(auth()->user())->count(),
+            'assignments_recovery_time_expired' => Assignment::where('created_at', '<', Carbon::now()->subHours(48))->accessibleBy(auth()->user())->count(),
         ]);
     }
 
@@ -66,9 +66,9 @@ class DashboardController extends Controller
         // $this->authorize('viewAny', User::class);
 
         return $this->responseSuccess(null, [
-            'total_insurers' => ['value' => Entity::where('entity_type_id', EntityType::where('code', EntityTypeEnum::INSURER)->first()->id)->count()],
-            'active_insurers' => ['value' => Entity::where('entity_type_id', EntityType::where('code', EntityTypeEnum::INSURER)->first()->id)->where('status_id', Status::where('code', StatusEnum::ACTIVE)->first()->id)->count()],
-            'inactive_insurers' => ['value' => Entity::where('entity_type_id', EntityType::where('code', EntityTypeEnum::INSURER)->first()->id)->where('status_id', Status::where('code', StatusEnum::INACTIVE)->first()->id)->count()],
+            'total_insurers' => ['value' => Entity::where('entity_type_id', EntityType::where('code', EntityTypeEnum::INSURER)->first()->id)->accessibleBy(auth()->user())->count()],
+            'active_insurers' => ['value' => Entity::where('entity_type_id', EntityType::where('code', EntityTypeEnum::INSURER)->first()->id)->where('status_id', Status::where('code', StatusEnum::ACTIVE)->first()->id)->accessibleBy(auth()->user())->count()],
+            'inactive_insurers' => ['value' => Entity::where('entity_type_id', EntityType::where('code', EntityTypeEnum::INSURER)->first()->id)->where('status_id', Status::where('code', StatusEnum::INACTIVE)->first()->id)->accessibleBy(auth()->user())->count()],
         ]);
     }
 
@@ -80,9 +80,9 @@ class DashboardController extends Controller
         // $this->authorize('viewAny', User::class);
 
         return $this->responseSuccess(null, [
-            'total_repairers' => ['value' => Entity::where('entity_type_id', EntityType::where('code', EntityTypeEnum::REPAIRER)->first()->id)->count()],
-            'active_repairers' => ['value' => Entity::where('entity_type_id', EntityType::where('code', EntityTypeEnum::REPAIRER)->first()->id)->where('status_id', Status::where('code', StatusEnum::ACTIVE)->first()->id)->count()],
-            'inactive_repairers' => ['value' => Entity::where('entity_type_id', EntityType::where('code', EntityTypeEnum::REPAIRER)->first()->id)->where('status_id', Status::where('code', StatusEnum::INACTIVE)->first()->id)->count()],
+            'total_repairers' => ['value' => Entity::where('entity_type_id', EntityType::where('code', EntityTypeEnum::REPAIRER)->first()->id)->accessibleBy(auth()->user())->count()],
+            'active_repairers' => ['value' => Entity::where('entity_type_id', EntityType::where('code', EntityTypeEnum::REPAIRER)->first()->id)->where('status_id', Status::where('code', StatusEnum::ACTIVE)->first()->id)->accessibleBy(auth()->user())->count()],
+            'inactive_repairers' => ['value' => Entity::where('entity_type_id', EntityType::where('code', EntityTypeEnum::REPAIRER)->first()->id)->where('status_id', Status::where('code', StatusEnum::INACTIVE)->first()->id)->accessibleBy(auth()->user())->count()],
         ]);
     }
 
@@ -94,9 +94,9 @@ class DashboardController extends Controller
         // $this->authorize('viewAny', User::class);
 
         return $this->responseSuccess(null, [
-            'total_vehicles' => ['value' => Vehicle::count()],
-            'active_vehicles' => ['value' => Vehicle::where('status_id', Status::where('code', StatusEnum::ACTIVE)->first()->id)->count()],
-            'inactive_vehicles' => ['value' => Vehicle::where('status_id', Status::where('code', StatusEnum::INACTIVE)->first()->id)->count()],
+            'total_vehicles' => ['value' => Vehicle::accessibleBy(auth()->user())->count()],
+            'active_vehicles' => ['value' => Vehicle::where('status_id', Status::where('code', StatusEnum::ACTIVE)->first()->id)->accessibleBy(auth()->user())->count()],
+            'inactive_vehicles' => ['value' => Vehicle::where('status_id', Status::where('code', StatusEnum::INACTIVE)->first()->id)->accessibleBy(auth()->user())->count()],
         ]);
     }
 }
