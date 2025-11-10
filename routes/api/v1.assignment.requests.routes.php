09d7296574721ff5b0a2755a8bf9e0173a2ff947
fanebrahima <brahimafane06@gmail.com>
2025-11-10 21:57:45 +0000

    Update

--- routes/api/v1.assignment.requests.routes.php
+++ routes/api/v1.assignment.requests.routes.php
@@ -5,8 +5,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->name('assignment.requests.')->group(function () {
     Route::get('/', [AssignmentRequestController::class, 'index'])->name('index');
+    Route::post('/', [AssignmentRequestController::class, 'store'])->name('store');
     Route::get('/{assignment_request}', [AssignmentRequestController::class, 'show'])->name('show');
     Route::put('/{assignment_request}', [AssignmentRequestController::class, 'update'])->name('update');
     Route::delete('/{assignment_request}', [AssignmentRequestController::class, 'destroy'])->name('destroy');
 }); 
-Route::post('assignment-requests', [AssignmentRequestController::class, 'store'])->name('store');
