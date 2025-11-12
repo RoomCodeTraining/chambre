df9c1a9d16e21c3aadf419b5259312d1a754db2b
fanebrahima <brahimafane06@gmail.com>
2025-11-12 14:51:42 +0000

    Update

--- app/Builders/Assignment/AssignmentBuilder.php
+++ app/Builders/Assignment/AssignmentBuilder.php
@@ -78,13 +78,13 @@ class AssignmentBuilder extends Builder
         }
 
         if ($user->isRepairerAdmin()) {
-            return $this->where('repairer_id', $user->entity_id);
-            // return $this->where('repairer_id', $user->entity_id)->whereNotIn('status_id', Status::whereIn('code', [StatusEnum::OPENED, StatusEnum::REALIZED])->pluck('id'));
+            // return $this->where('repairer_id', $user->entity_id);
+            return $this->where('repairer_id', $user->entity_id)->whereNotIn('status_id', Status::whereIn('code', [StatusEnum::OPENED, StatusEnum::REALIZED])->pluck('id'));
         }
 
         if ($user->isRepairerStandardUser()) {
-            return $this->where('repairer_id', $user->entity_id);
-            // return $this->where('repairer_id', $user->entity_id)->whereNotIn('status_id', Status::whereIn('code', [StatusEnum::OPENED, StatusEnum::REALIZED])->pluck('id'));
+            // return $this->where('repairer_id', $user->entity_id);
+            return $this->where('repairer_id', $user->entity_id)->whereNotIn('status_id', Status::whereIn('code', [StatusEnum::OPENED, StatusEnum::REALIZED])->pluck('id'));
         }
 
         if ($user->isClient()) {
