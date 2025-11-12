8e22d0acd2e27c66209943451368ff2b68fbc5b6
fanebrahima <brahimafane06@gmail.com>
2025-11-12 02:12:27 +0000

    Update

--- routes/api/v1.assignment.messages.routes.php
+++ routes/api/v1.assignment.messages.routes.php
@@ -4,9 +4,9 @@ use App\Http\Controllers\API\AssignmentMessageController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->name('assignment-messages.')->group(function () {
-    Route::get('/', [AssignmentMessageController::class, 'index'])->name('assignment-messages.index');
-    Route::post('/', [AssignmentMessageController::class, 'store'])->name('assignment-messages.store');
-    Route::get('/{assignment_message}', [AssignmentMessageController::class, 'show'])->name('assignment-messages.show');
-    Route::put('/{assignment_message}', [AssignmentMessageController::class, 'update'])->name('assignment-messages.update');
-    Route::delete('/{assignment_message}', [AssignmentMessageController::class, 'destroy'])->name('assignment-messages.destroy');
+    Route::get('/', [AssignmentMessageController::class, 'index'])->name('index');
+    Route::post('/', [AssignmentMessageController::class, 'store'])->name('store');
+    Route::get('/{assignment_message}', [AssignmentMessageController::class, 'show'])->name('show');
+    Route::put('/{assignment_message}', [AssignmentMessageController::class, 'update'])->name('update');
+    Route::delete('/{assignment_message}', [AssignmentMessageController::class, 'destroy'])->name('destroy');
 });
