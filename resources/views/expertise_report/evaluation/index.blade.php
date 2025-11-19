bf4b9ac5b9769348483c2b3264765068dced726f
fanebrahima <brahimafane06@gmail.com>
2025-11-19 02:16:13 +0000

    Update

--- resources/views/expertise_report/evaluation/index.blade.php
+++ resources/views/expertise_report/evaluation/index.blade.php
@@ -214,15 +214,15 @@
             <h2 style="text-align: center;">{{ mb_strtoupper($assignment?->vehicle?->license_plate ?? '')}}</h2>
         </p>
 
-        @if($assignment?->status_id == 3 || $assignment?->status_id == 4 || $assignment?->status_id == 5)
+        @if($assignment?->status_id == 3 || $assignment?->status_id == 4 || $assignment?->status_id == 5 || $assignment?->status_id == 6 || $assignment?->status_id == 7 || $assignment?->status_id == 8)
             <div class="watermark">Provisoire</div>
         @endif
 
-        @if($assignment?->status_id == 8)
+        @if($assignment?->status_id == 11)
             <div class="watermark">Annulé</div>
         @endif
 
-        @if($assignment?->status_id == 10)
+        @if($assignment?->status_id == 13)
             <div class="watermark">Supprimé</div>
         @endif
 
