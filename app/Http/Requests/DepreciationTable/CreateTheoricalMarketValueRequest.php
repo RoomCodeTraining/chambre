b6f7cf498fe8188ee3141cd64980681edb6f36e4
fanebrahima <brahimafane06@gmail.com>
2025-11-20 10:23:38 +0000

    Update

--- app/Http/Requests/DepreciationTable/CreateTheoricalMarketValueRequest.php
+++ app/Http/Requests/DepreciationTable/CreateTheoricalMarketValueRequest.php
@@ -35,6 +35,20 @@ class CreateTheoricalMarketValueRequest extends FormRequest
             'first_entry_into_circulation_date.date_format' => 'Le format de la date est invalide.',
             'expertise_date.date' => 'La date est invalide.',
             'expertise_date.date_format' => 'Le format de la date est invalide.',
+            'expertise_date.after' => 'La date d\'expertise doit être après la date de première mise en circulation.',
+            'expertise_date.before' => 'La date d\'expertise doit être avant la date de demain.',
+            'first_entry_into_circulation_date.after' => 'La date de première mise en circulation doit être après la date d\'aujourd\'hui.',
+            'first_entry_into_circulation_date.before' => 'La date de première mise en circulation doit être avant la date de demain.',
+            'vehicle_genre_id.required' => 'Le genre de véhicule est requis.',
+            'vehicle_genre_id.exists' => 'Le genre de véhicule est invalide.',
+            'vehicle_energy_id.required' => 'L\'énergie du véhicule est requise.',
+            'vehicle_energy_id.exists' => 'L\'énergie du véhicule est invalide.',
+            'vehicle_new_value.required' => 'La valeur neuve du véhicule est requise.',
+            'vehicle_new_value.integer' => 'La valeur neuve du véhicule doit être un nombre entier.',
+            'vehicle_new_value.min' => 'La valeur neuve du véhicule doit être supérieure ou égale à 0.',
+            'vehicle_mileage.required' => 'Le kilométrage du véhicule est requis.',
+            'vehicle_mileage.integer' => 'Le kilométrage du véhicule doit être un nombre entier.',
+            'vehicle_mileage.min' => 'Le kilométrage du véhicule doit être supérieure ou égale à 0.',
         ];
     }
 }
