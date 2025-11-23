0874596528e93a8e0cad0a00c9f1f501363bac5e
fanebrahima <brahimafane06@gmail.com>
2025-11-23 19:30:44 +0000

    Update

--- database/migrations/2025_10_22_115227_add_is_for_work_sheet_to_shock_works_table.php
+++ database/migrations/2025_10_22_115227_add_is_for_work_sheet_to_shock_works_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::table('shock_works', function (Blueprint $table) {
             $table->boolean('is_for_work_sheet')->default(false);
+            $table->boolean('quote_validated')->default(false);
         });
     }
 
@@ -23,6 +24,7 @@ return new class extends Migration
     {
         Schema::table('shock_works', function (Blueprint $table) {
             $table->dropColumn('is_for_work_sheet');
+            $table->dropColumn('quote_validated');
         });
     }
 };
