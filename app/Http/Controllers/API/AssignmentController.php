3f71853726394c6efa2f367ff43eeefdaf876ac9
fanebrahima <brahimafane06@gmail.com>
2025-11-23 20:43:30 +0000

    Update

--- app/Http/Controllers/API/AssignmentController.php
+++ app/Http/Controllers/API/AssignmentController.php
@@ -2825,7 +2825,7 @@ class AssignmentController extends Controller
      */
     public function validateQuoteWithConditionsByExpert($id): JsonResponse
     {
-        $assignment = Assignment::with('shocks:id,is_validated', 'shocks.shockWorks:id,is_validated', 'shocks.workforces:id,is_validated')->findOrFail(Assignment::keyFromHashId($id));
+        $assignment = Assignment::with('shocks:id,quote_validated', 'shocks.shockWorks:id,quote_validated', 'shocks.workforces:id,quote_validated')->findOrFail(Assignment::keyFromHashId($id));
 
         if($assignment->status_id == Status::where('code', StatusEnum::PENDING_FOR_REPAIRER_QUOTE)->first()->id || $assignment->status_id == Status::where('code', StatusEnum::PENDING_FOR_REPAIRER_QUOTE_VALIDATION)->first()->id){
             $assignment->update([
