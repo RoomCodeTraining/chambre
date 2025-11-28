28a9c64ea0e1bc186caa939d026661c1be2474ec
fanebrahima <brahimafane06@gmail.com>
2025-11-28 12:56:27 +0000

    Update

--- app/Http/Controllers/API/PermissionController.php
+++ app/Http/Controllers/API/PermissionController.php
@@ -46,7 +46,7 @@ class PermissionController extends Controller
     public function list(Request $request): AnonymousResourceCollection
     {
         $permissions = AppPermission::with(['roles'])
-            ->accessibleBy($request->user())
+            // ->accessibleBy($request->user())
             ->latest('created_at')
             ->dynamicPaginate();
 
